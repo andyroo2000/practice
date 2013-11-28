8d0a0d95aeef1bf334109f7760c48e5e62804f2c
Andrew Landry <andrewlandry@gmail.com>
2013-11-28 13:56:47 -0500

    complete first_factorial

---
+++ Vanillajs/coderbyte/easyDifficulty/02-first_factorial.js
@@ -0,0 +1,24 @@
+/*
+
+Using the JavaScript language, have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it (ie. if num = 4, return (4 * 3 * 2 * 1)). For the test cases, the range will be between 1 and 18. 
+
+Correct Sample Outputs:
+
+Input = 4Output = 24
+Input = 8Output = 40320
+
+*/
+
+var FirstFactorial = function(num) {
+  if (num < 0) {
+    return -1;
+  } else if (num === 0) {
+     return 1;
+  } else {
+    return num * FirstFactorial(num - 1);
+  }
+};
+
+
+console.log(FirstFactorial(4));
+console.log(FirstFactorial(8));
