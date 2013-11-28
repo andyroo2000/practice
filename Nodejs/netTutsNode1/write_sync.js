7011e129594d285f6ff03bcbf7202b8d46038908
Andrew Landry <andrewlandry@gmail.com>
2013-11-28 12:26:04 -0500

    removed files

--- Nodejs/netTutsNode1/write_sync.js
+++
@@ -1,7 +0,0 @@
-var fs = require("fs");
-console.log("Starting");
-fs.writeFile("./files/write_sync.txt", "Hello world!", function (error) {
-  console.log("Written file");
-});
-console.log("Finished!");
-
