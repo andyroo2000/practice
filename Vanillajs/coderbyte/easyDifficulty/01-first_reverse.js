c8eaf3a3eff20fbf7e0cc47679b48b60a24c3db2
Andrew Landry <andrewlandry@gmail.com>
2013-11-28 13:38:53 -0500

    complete first_reverse

--- Vanillajs/coderbyte/easyDifficulty/01-first_reverse.js
+++ Vanillajs/coderbyte/easyDifficulty/01-first_reverse.js
@@ -1,1 +1,14 @@
-Using the JavaScript language, have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. 
+// Using the JavaScript language, have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. 
+
+// Correct Sample Outputs
+// 
+// Input = "coderbyte"Output = "etybredoc"
+// Input = "I Love Code"Output = "edoC evoL I"
+
+
+var FirstReverse = function(str) {
+  var theSplit = str.split("");
+  return theSplit.reverse().join("");
+}
+
+console.log(FirstReverse("coderbyte"));
