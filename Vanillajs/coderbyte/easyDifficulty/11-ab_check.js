b854d0d1b74195d60728e1084da4ec2d80463b19
Andrew Landry <andrewlandry@gmail.com>
2013-11-29 16:19:35 -0500

    complete ab_check

--- Vanillajs/coderbyte/easyDifficulty/11-ab_check.js
+++ Vanillajs/coderbyte/easyDifficulty/11-ab_check.js
@@ -8,6 +8,17 @@ Input = "Laura sobs"   Output = "true"
 
 */
 
+var ABCheck = function(str) {
+  var result = false;
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] === "a") {
+      if (str[i + 4] === "b") {
+        result = true;
+      }
+    }
+  }
+  return result;
+};
 
-
-
+console.log(ABCheck("after badly"));
+console.log(ABCheck("Laura sobs"));
