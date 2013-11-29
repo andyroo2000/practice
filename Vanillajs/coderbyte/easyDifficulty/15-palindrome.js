83fb2d24c1638454ce0ddb6257f9c175a8420e37
Andrew Landry <andrewlandry@gmail.com>
2013-11-29 17:58:58 -0500

    complete palindrome

--- Vanillajs/coderbyte/easyDifficulty/15-palindrome.js
+++ Vanillajs/coderbyte/easyDifficulty/15-palindrome.js
@@ -8,6 +8,17 @@ Input = "eye"                Output = "true"
 
 */
 
+var Palindrome = function(str) {
+  var str = str.split(" ").join("");
+  for (var i = 0; i < str.length/2; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false
+    }
+    return true;
+  }
+};
 
-
-
+console.log(Palindrome("never odd or even"));    // true
+console.log(Palindrome("eye"));                  // true
+console.log(Palindrome("racecar"));              // true
+console.log(Palindrome("racecars"));             // false
