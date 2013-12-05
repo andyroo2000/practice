f5ad7e1d3af8b7a9e8506127c7dc33fe3f6842fe
Andrew Landry <andrewlandry@gmail.com>
2013-12-05 00:30:51 -0500

    complete swap_case

--- Vanillajs/coderbyte/easyDifficulty/24-swap_case.js
+++ Vanillajs/coderbyte/easyDifficulty/24-swap_case.js
@@ -8,6 +8,37 @@ Input = "Sup DUDE!!?"  Output = "sUP dude!!?"
 
 */
 
+var isUpperCase = function(letter) {
+  if ((/[A-Z]/).test(letter)) {
+    return true;
+  }
+  return false;
+};
+
+var isLowerCase = function(letter) {
+  if ((/[a-z]/).test(letter)) {
+    return true;
+  }
+  return false;
+};
 
 
+var SwapCase = function(str) {
+  var strArray = str.split("");
+  for (var i = 0; i < strArray.length; i++) {
+    if (isUpperCase(strArray[i])) {
+      strArray[i] = strArray[i].toLowerCase();
+    }
+    else if (isLowerCase(strArray[i])) {
+      strArray[i] = strArray[i].toUpperCase();
+    }
+  }
+  return strArray.join("");
+};
 
+
+console.log(SwapCase("Hello World"));  // hELLO wORLD
+console.log(SwapCase("Hello-LOL"));    // hELLO-lol
+console.log(SwapCase("Sup DUDE!!?"));  // sUP dude!!?
+
+// console.log(isLowerCase("A"));
