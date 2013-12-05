a5e8b9ed893150c0d0fdc198d212627c6a9ecf82
Andrew Landry <andrewlandry@gmail.com>
2013-12-05 00:02:17 -0500

    complete dash_insert

--- Vanillajs/coderbyte/easyDifficulty/23-dash_insert.js
+++ Vanillajs/coderbyte/easyDifficulty/23-dash_insert.js
@@ -8,6 +8,17 @@ Input = 56730  Output = "567-30"
 
 */
 
+var DashInsert = function(num) {
+  var numArray = num.toString().split("");
+  for (var i = numArray.length - 1; i > 0; i--) {
+    if ( (numArray[i] % 2 !== 0) && (numArray[i - 1] % 2 !== 0) ) {
+      numArray.splice(i, 0, "-");
+    }
+  }
+  return numArray.join("");
+};
 
 
-
+console.log(DashInsert(454793));  // 4547-9-3
+console.log(DashInsert(99946));   // 9-9-946
+console.log(DashInsert(56730));   // 567-30
