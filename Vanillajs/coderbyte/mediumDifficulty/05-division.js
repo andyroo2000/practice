3ed6965d4eb9183ecc2801cd9b97db53f3868528
Andrew Landry <andrewlandry@gmail.com>
2013-12-10 21:18:30 -0500

    complete division

--- Vanillajs/coderbyte/mediumDifficulty/05-division.js
+++ Vanillajs/coderbyte/mediumDifficulty/05-division.js
@@ -1,13 +1,29 @@
 /*
-
 Have the function Division(num1,num2) take both parameters being passed and return the Greatest Common Factor. That is, return the greatest number that evenly goes into both numbers with no remainder. For example: 12 and 16 both are divisible by 1, 2, and 4 so the output should be 4. The range for both parameters will be from 1 to 10^3.
 
 Correct Sample Outputs
-Input = 7 & num2 = 3    Output = 1
-Input = 36 & num2 = 54  Output = 18
-
+Input = 7 & 3    Output = 1
+Input = 36 & 54  Output = 18
 */
 
+var lowestNumberFirst = function(num1, num2) {
+  if (num1 > num2) {
+    return [num2, num1];
+  }
+  return [num1, num2];
+}
 
+var Division = function(num1, num2) {
+  var sortedNumbers = lowestNumberFirst(num1, num2);
+  var result = 1;
+  for (var i = 2; i < num1; i++) {
+    if ((num1 % i === 0) && (num2 % i === 0)) {
+      result = i;
+    }
+  }
+  return result;
+};
 
-
+console.log(Division(12, 16));       // 4
+console.log(Division(7, 3));         // 1
+console.log(Division(36, 54));       // 18
