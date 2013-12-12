c3aa1f020c634dd8d24cb673518920218e636f48
Andrew Landry <andrewlandry@gmail.com>
2013-12-12 00:10:15 -0500

    complete palindrome_two

--- Vanillajs/coderbyte/mediumDifficulty/04-palindrome_two.js
+++ Vanillajs/coderbyte/mediumDifficulty/04-palindrome_two.js
@@ -8,6 +8,25 @@ Input = "A war at Tarawa!"  Output = "true"
 
 */
 
+var stripString = function(str) {
+  var newString = str.replace(/[^a-zA-Z\w]/g,"");
+  return newString.toLowerCase();
+};
+
+var PalindromeTwo = function(str) {
+  var formattedString = stripString(str);
+  for (var i = 0; i < formattedString.length / 2; i++) {
+    if (formattedString[i] !== formattedString[formattedString.length - 1 - i]) {
+      return false;
+    }
+    return true;
+  }
+};
 
 
+console.log(PalindromeTwo("Anne, I vote more cars race Rome-to-Vienna"));  // true
+console.log(PalindromeTwo("Noel - sees Leon"));                            // true
+console.log(PalindromeTwo("A war at Tarawa!"));                            // true
 
+console.log(PalindromeTwo("racecars"));                                    // false
+console.log(PalindromeTwo("A war at Tarawan"));                            // false
